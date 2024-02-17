8bf11be0870b6b6a4e53798274752d7403a15ace
Drew Sirenko <andrewsirenko@gmail.com>
2024-02-17 11:46:59 -0500

    Solve bandit 11-12 on OverTheWire

---
+++ bandit/bandit12.sh
@@ -0,0 +1,13 @@
+cat data.txt # shows data2.bin
+xxd -r data.txt data
+file data # shows it is gzip max compression
+cat data | gzip -d > data2
+file data2 # shows bzip2
+cat data2 | bzip2 -d > data4.bin
+file data4.bin # shows gzip
+cat data4.bin | gzip -d > data5.bin
+file data5.bin # shows tar archive
+tar -xvf data5.bin > data6.bin
+file data6.bin # shows bzip2
+cat data6.bin | bzip2 -d > data8.bin
+cat data8.bin | gzip -d
